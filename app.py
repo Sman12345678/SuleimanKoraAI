fe8a22729ea4c203fa6aef108c20cbbdcc07c375
cyrustet <cyrustet@gmail.com>
2024-09-13 20:47:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 import google.generativeai as genai
 
+# Load env from .env file
+load_dotenv()
+
 app = Flask(__name__)
 
-# Configure Gemini AI, rem to create a .env 
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+# Gemini AI
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-# the model
+# Create the model
 generation_config = {
     "temperature": 0.9,
     "top_p": 1,
@@ -17,14 +21,14 @@ generation_config = {
 
 # System instruction
 system_instruction = """
-You are an AI assistant named Kora. You are made by Suleiman . He's your owner and creator. Your primary function is to assist users with various tasks and answer their questions.
+You are an AI assistant named Kora. Your owner, creator and founder is Suleiman Your primary function is to assist users with various tasks and answer their questions.
 You are knowledgeable, helpful, and always strive to provide accurate and relevant information.
 If you're unsure about something, you're not afraid to admit it and suggest where the user might find more information.
 You have a friendly and professional demeanor, and you aim to make interactions pleasant and productive for the user.
 """
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-pro",  # Update this to the latest available model
+    model_name="gemini-1.5-pro",  # you can change to select model
     generation_config=generation_config,
 )
 
@@ -36,13 +40,13 @@ def koraai():
     
     try:
         chat_session = model.start_chat(history=[])
-        # Apply system instruction we made
+        # Apply system instruction
         chat_session.send_message(system_instruction)
-        # Send user query, you can change for customisation
+        # Send user answer
         response = chat_session.send_message(query)
         return jsonify({"response": response.text})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
